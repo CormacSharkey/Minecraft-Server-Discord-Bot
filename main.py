9dca5d77fbba7b74651528dce94407acf3f54672
CormacSharkey <csharkey@tcd.ie>
2024-03-31 00:01:53 +0000

    fixing issue with permissions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
