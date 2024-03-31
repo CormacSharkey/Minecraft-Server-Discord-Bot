d4321309276b5d19676a5f120fdea404a4fab6a9
Aaron Dinesh <aarondinesh2002@gmail.com>
2024-03-31 02:22:46 +0100

    Marco...Polo!

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.messages = True
+intents.message_content = True
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command
+async def marco(ctx):
+    await ctx.send("polo")
+
+
+bot.run(TOKEN)
+
+# client = discord.Client(intents=intents)
+
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+
+# client.run(TOKEN)
