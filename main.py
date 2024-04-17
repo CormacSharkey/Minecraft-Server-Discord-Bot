99a1df2b21c02d20749b6cab2283dcb5b9bca0c4
CormacSharkey <csharkey@tcd.ie>
2024-04-17 23:27:28 +0100

    adding YT audio playback in vc functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import time
 
+import yt_dlp
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -8,6 +10,13 @@ from selenium.webdriver.chrome.options import Options
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+FILE_LOCATION = "C:/Users/corma/Desktop/Code/Python/Repos/Minecraft-Server-Discord-Bot/Audios/"
+
+def download_audio(link):
+    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': FILE_LOCATION+'jangle.mp3'}) as video:
+        video.download(link)    
+        print("Successfully Downloaded Link " + link)
 
 # Add uBlock Origin to the Chrome Driver
 chop = webdriver.ChromeOptions()
@@ -78,15 +87,42 @@ async def website(ctx, arg):
     except:
         await ctx.send("Womp womp spell better")
 
+# @bot.command(name="connect")
+# async def connect(ctx):
+#     # Gets voice channel of message author
+#     voice_channel = ctx.author.voice.channel
+#     # Connect to voice chat if author is in one
+#     if (voice_channel):
+#         vc = await voice_channel.connect()
+#     else:
+#         await ctx.send(str(ctx.author.name) + "is not in a channel.")
+
 @bot.command(name="jingle")
 async def jingle(ctx):
     # Gets voice channel of message author
     voice_channel = ctx.author.voice.channel
     if (voice_channel):
         vc = await voice_channel.connect()
+        # ctx.guild.voice_client.play(discord.FFmpegPCMAudio(executable="C:/FFMPEG/bin/ffmpeg.exe", source="Audios/jingle.m4a"))
         vc.play(discord.FFmpegPCMAudio(executable="C:/FFMPEG/bin/ffmpeg.exe", source="Audios/jingle.m4a"))
+        ctx.voice_client.disconnect()
     else:
         await ctx.send(str(ctx.author.name) + "is not in a channel.")
+
+@bot.command(name="play")
+async def play(ctx, arg):
+    download_audio(arg)
+
+    # Gets voice channel of message author
+    voice_channel = ctx.author.voice.channel
+    if (voice_channel):
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio(executable="C:/FFMPEG/bin/ffmpeg.exe", source="Audios/jangle.mp3"))
+        ctx.voice_client.disconnect()
+    else:
+        await ctx.send(str(ctx.author.name) + "is not in a channel.")
+
+
 
 @bot.event
 async def on_message(message):
@@ -105,7 +141,7 @@ async def on_ready():
     print(bot.guilds[0].name)
     print("----------")
 
-bot.run(TOKEN)
+bot.run(TOKEN)     
 
 # client = discord.Client(intents=intents)
 
