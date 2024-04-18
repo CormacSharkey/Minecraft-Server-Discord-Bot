5806fe30250babde4cb91ea72ada32249b08e15c
CormacSharkey <csharkey@tcd.ie>
2024-04-18 23:19:39 +0100

    adding YT searching code and comments

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Youtube Searching Mock Code
+# from youtube_search import YoutubeSearch
+
+# results = YoutubeSearch('a-ha', max_results=10).to_dict()
+# for i in range(0, len(results)):
+#     print(results[i].get("title") + "\t")
+#     print("https://www.youtube.com/" + results[i].get("url_suffix"))
+    
+
+# print(results[0].get("title"))
+
+
+
 FILE_LOCATION = "C:/Users/corma/Desktop/Code/Python/Repos/Minecraft-Server-Discord-Bot/Audios/"
 
 def download_audio(link):
     with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': FILE_LOCATION+'jangle.mp3'}) as video:
         video.download(link)    
         print("Successfully Downloaded Link " + link)
+
 
 # Add uBlock Origin to the Chrome Driver
 chop = webdriver.ChromeOptions()
@@ -28,24 +42,28 @@ driver = webdriver.Chrome(options = chop)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Set the bot's intents
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 intents.messages = True
 intents.message_content = True
 
-
+# Specify the bot's command prefix and intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Bot Command - when user sends "!marco", bot responds with "polo"
 @bot.command(name='marco')
 async def marco(ctx):
     await ctx.send("polo")
 
+# Bot Command - when user sends "!search [arg]", the bot searches a MC wiki for the arg value as a recipe
+# Returns an image of the recipe(s) or an error message for spelling
 @bot.command(name='search')
 async def website(ctx, arg):
     # Open the Minecraft Crafting Guide website
     driver.get('https://www.minecraftcraftingguide.net/')
-    # Wait for 2 seconds to load it
+    # Wait for 0.1 seconds to load it
     driver.implicitly_wait(0.1)
     # Dealing with a cookie consent popup (not needed with uBlock Origin)
     # popUp = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[1]/div[2]/div[2]/button[1]").click()
@@ -62,7 +80,7 @@ async def website(ctx, arg):
     # Change the webpage height and width to max
     height = driver.execute_script('return document.documentElement.scrollHeight')
     width  = driver.execute_script('return document.documentElement.scrollWidth')
-    driver.set_window_size(width, height)  # the trick
+    driver.set_window_size(width, height)
     time.sleep(0.1)
 
     # Exception Handling - to catch when the search argument isn't valid or no search results
@@ -87,6 +105,8 @@ async def website(ctx, arg):
     except:
         await ctx.send("Womp womp spell better")
 
+# Bot Command - when user sends "!connect", the bot will connect to the same voice chat as the user
+# Returns an error message if the user is not in a voice chat
 # @bot.command(name="connect")
 # async def connect(ctx):
 #     # Gets voice channel of message author
@@ -97,6 +117,8 @@ async def website(ctx, arg):
 #     else:
 #         await ctx.send(str(ctx.author.name) + "is not in a channel.")
 
+# Bot Command - when user sends "!jingle", the bot will connect to the user's voice chat and play "Aww Creeper!" audio
+# Returns an error message if the user is not in a voice chat
 @bot.command(name="jingle")
 async def jingle(ctx):
     # Gets voice channel of message author
@@ -109,6 +131,7 @@ async def jingle(ctx):
     else:
         await ctx.send(str(ctx.author.name) + "is not in a channel.")
 
+# Bot Command - when user sends "!play [arg]", the bot downloads the audio of the provided arg Youtube video, joins voice chat and plays it
 @bot.command(name="play")
 async def play(ctx, arg):
     download_audio(arg)
@@ -122,8 +145,7 @@ async def play(ctx, arg):
     else:
         await ctx.send(str(ctx.author.name) + "is not in a channel.")
 
-
-
+# Bot Event - when the user sends "Hello", the bot sends back "Hello there user!", where "user" is the user's name
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -133,6 +155,7 @@ async def on_message(message):
         await message.channel.send("Hello there " + message.author.name + "!")
     await bot.process_commands(message)
 
+# Bot Event - when the bot is ready, it prints out its self info and server info
 @bot.event
 async def on_ready():
     print("Logged in as")
