99506416808a357ddf8c906c14fe6d7e3f311b15
CormacSharkey <csharkey@tcd.ie>
2024-08-13 12:52:18 +0100

    removing unused code in comments

--- botCommands.py
+++ botCommands.py
@@ -112,40 +112,6 @@ async def disconnect(ctx, arg):
     # Disconnect the bot from the voice channel its in
     await ctx.voice_client.disconnect()
 
-# @bot.event
-# async def on_voice_state_update(member, before, after):
-#     if (before.channel):
-#         if bot.user in before.channel.members and len([m for m in before.channel.members if not m.bot]) == 0:
-#             channel = discord.utils.get(bot.voice_clients, channel=before.channel)
-#             await channel.disconnect()
-    # ctx = await bot.get_context(before)
-
-    # def check_VC():
-    #     voice = set()
-    #     for v in ctx.guild.voice_channels:
-    #         for member in v.members:
-    #             voice.add(member.id)
-    #     return len(voice) == 0
-    
-    # if (check_VC()):
-    #     await ctx.voice_client.disconnect()
-
-
-
-# Bot Command - when user sends "!jingle", the bot will connect to the user's voice chat and play "Aww Creeper!" audio
-# Returns an error message if the user is not in a voice chat
-# @bot.command(name="jingle")
-# async def jingle(ctx):
-#     # Gets voice channel of message author
-#     voice_channel = ctx.author.voice.channel
-#     if (voice_channel):
-#         vc = await voice_channel.connect()
-#         # ctx.guild.voice_client.play(discord.FFmpegPCMAudio(executable="C:/FFMPEG/bin/ffmpeg.exe", source="Audios/jingle.m4a"))
-#         vc.play(discord.FFmpegPCMAudio(executable="C:/FFMPEG/bin/ffmpeg.exe", source="Audios/jingle.m4a"))
-#         ctx.voice_client.disconnect()
-#     else:
-#         await ctx.send(str(ctx.author.name) + "is not in a channel.")
-
 # @bot.command(name="play")
 async def play(ctx, arg):
 
